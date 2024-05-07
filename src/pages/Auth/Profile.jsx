448b5a06f1b11a9e891d81b72d31cf39b332dde5
gerardaragones <gearci@fp.insjoaquimmir.cat>
2024-05-07 21:06:15 +0200

    img

--- src/pages/Auth/Profile.jsx
+++ src/pages/Auth/Profile.jsx
@@ -6,44 +6,46 @@ import { useState, useEffect } from 'react'
 
 export default function Profile() {
 
-	Logger.debug("Profile page")
+    Logger.debug("Profile page")
 
-	const { authToken } = useUserContext()
-	const { authService, userService } = useServicesContext()
+    const { authToken } = useUserContext()
+    const { authService, userService } = useServicesContext()
 
-	const [ profile, setProfile ] = useState({})
-	
-	useEffect(() => {
-		(async () => {
-			// Auth 
-			try {
-				const data = await userService.getOne(authToken)
-				console.log(data);
-				setProfile(data.user)
-				return data
-			} catch (error) {
-				Logger.error(error.message)
-				alert("ERROR carregant perfil d'usuari/a... :-(")
-			}
-		})()
-	}, [])
+    const [ profile, setProfile ] = useState({})
 
-	return (
-		<Layout>
-			<section id="profile" className="w-75 m-auto">
-				<h1>Perfil</h1>
-				{ profile 
-					? 
-					<>
-						<h2 className="mt-4">Nombre:</h2>
-						<p>{profile.name} {profile.surname}</p>
-						<h2 className="mt-4">Correo electónico:</h2>
-						<p>{profile.email}</p>
-					</>
-					: 
-					<p>Carregant perfil...</p>
-				}
-			</section>
-		</Layout>
-	)
+    useEffect(() => {
+        (async () => {
+            // Auth 
+            try {
+                const data = await userService.getOne(authToken)
+                console.log(data);
+                setProfile(data.user)
+                return data
+            } catch (error) {
+                Logger.error(error.message)
+                alert("ERROR carregant perfil d'usuari/a... :-(")
+            }
+        })()
+    }, [])
+
+    return (
+        <Layout>
+            <section id="profile" className="w-75 m-auto">
+                <h1>Perfil</h1>
+                { profile 
+                    ? 
+                    <>
+                        <h2 className="mt-4">Nombre:</h2>
+                        <p>{profile.name} {profile.surname}</p>
+                        <h2 className="mt-4">Correo electónico:</h2>
+                        <p>{profile.email}</p>
+                        <h2 className="mt-4">Imatge de perfil:</h2>
+                        {profile.pfp && <img src={profile.pfp} alt="Imatge de perfil" />}
+                    </>
+                    : 
+                    <p>Carregant perfil...</p>
+                }
+            </section>
+        </Layout>
+    )
 }

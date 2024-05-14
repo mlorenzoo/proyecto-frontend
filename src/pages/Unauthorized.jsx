196794c082e34984a223533d34e745ea5f876601
gerardaragones <gearci@fp.insjoaquimmir.cat>
2024-05-14 16:11:23 +0200

    b0.3

--- src/pages/Unauthorized.jsx
+++ src/pages/Unauthorized.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import { Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import Layout from '../components/Layout'
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const Unauthorized = () => {
   const navigate = useNavigate();
